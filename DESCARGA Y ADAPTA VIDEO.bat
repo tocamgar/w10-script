9a3105a620539968887fdcb48cef6dce2b45f87d
tocamgar <tocamgar@gmail.com>
2021-01-02 16:13:10 +0100

    Cambio nombres de variables en Descarga y adapta video.bat

--- DESCARGA Y ADAPTA VIDEO.bat
+++ DESCARGA Y ADAPTA VIDEO.bat
@@ -4,13 +4,13 @@ cd E:\V°deos\CURIOSAMENTE\
 ::for %%a in ("CAPITALISMO O SOCIALISMO_64_56_288p_140s_0.mp4") do ffprobe -v error -show_entries format=duration -sexagesimal -of default=noprint_wrappers=1:nokey=1 %%a>var.txt
 for %%a in (%1) do ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 %%a>var.txt
 ::"ffprobe -v error -show_entries format=duration -sexagesimal -of default=noprint_wrappers=1:nokey=1 KITE SURF .mp4"> aux.txt
-set /P _DURACI‡N_VIDEO=<var.txt 
-echo.DURACI‡N: %_DURACI‡N_VIDEO%
-SET /A _PARTES = %_DURACI‡N_VIDEO% / 140
-::set /A _ENTERO = %_DURACI‡N_VIDEO%
+set /P DURACI‡N_VIDEO=<var.txt 
+echo.DURACI‡N: %DURACI‡N_VIDEO%
+SET /A PARTES = %DURACI‡N_VIDEO% / 140
+::set /A _ENTERO = %DURACI‡N_VIDEO%
 ::ECHO.DURACI‡N: %_ENTERO%
-::SET /A _PARTES = %_ENTERO% / 140
-echo.PARTES: "%_PARTES%"
+::SET /A PARTES = %_ENTERO% / 140
+echo.PARTES: "%PARTES%"
 
 ::ffmpeg -i "https://youtu.be/MaKvo-3iWCo" -c copy "E:\V°deos\TODOS NAZIS.mp4"
 
